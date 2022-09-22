7dc72f9a3e8c7605615ad54d81642039d88de396
McKenzie Jones <mckenzbeck12@gmail.com>
2022-09-21 18:41:32 -0600

    Add buttons and draw effects

--- script.js
+++ script.js
@@ -1,22 +1,105 @@
+var currentColorSelection = "black";
+
+// function definitions
 function createGrid(x) {
   // set container styles
-  const container = document.querySelector("#container"); // Do I need this variable defined twice? See line 1.
+  const container = document.querySelector("#container");
   container.style.gridTemplateColumns = `repeat(${x}, auto)`;
 
   // create proper amount of X x Y cells
   for (let rows = 0; rows < x; rows++)
-    for (var cols = 0; cols < x; cols++) {
+    for (let cols = 0; cols < x; cols++) {
       const div = document.createElement("div");
       div.className = "cell";
-      div.addEventListener("mouseover", colorCell);
 
       document.querySelector("#container").appendChild(div);
     }
 }
 
-function colorCell(event) {
-  const cell = event.target;
-  cell.style.backgroundColor = "black";
+function colorCell(element, color) {
+  element.style.backgroundColor = color;
 }
 
-createGrid(50);
+function refreshGrid() {
+  const cells = document.querySelectorAll(".cell");
+
+  for (let i = 0; i < cells.length; i++) {
+    const singleCell = cells[i];
+    colorCell(singleCell, "white"); //colorCell function ran when looped through each individual cell and turns cell white - in other words, looped through all cells in grid and "clear" grid
+  }
+}
+
+function changeGridSize() {
+  const changeSizeString = prompt(
+    "How many boxes per side would you like your grid to have?"
+  );
+  const changeSizeNumber = parseInt(changeSizeString, 10);
+
+  // Removing previous cells before creating new sized grid.
+  const container = document.querySelector("#container");
+
+  // Make a copy of children
+  const childrenCopy = [...container.children];
+  const numCells = childrenCopy.length;
+  for (let i = 0; i < numCells; i++) {
+    const oldCell = childrenCopy[i];
+    oldCell.remove();
+  }
+
+  // Create the new grid!
+  createGrid(changeSizeNumber);
+}
+
+function generateRandomColor() {
+  const colors = ["black", "yellow", "green", "blue", "purple", "salmon"];
+  const randomDecimal = Math.random();
+  const randomNumber = colors.length * randomDecimal;
+  const roundedRandomWholeNumber = Math.round(randomNumber);
+  const randomColor = colors[roundedRandomWholeNumber];
+  return randomColor;
+}
+
+function handleMouseMove(event) {
+  if (event.target.className.includes("cell")) {
+    colorCell(event.target, currentColorSelection);
+  }
+}
+
+function handleSelection(event, color) {
+  if (!color) {
+    //if color selection is not black, choose a random color
+    currentColorSelection = generateRandomColor();
+    return;
+  }
+  currentColorSelection = color;
+}
+
+// Normal program flow - top to bottom
+
+// Create grid and setup mouse over event listeners
+createGrid(16);
+
+// Grab clear grid button and 'clear' cells (set color of each to white)
+const clearButton = document.querySelector("#clearGrid");
+clearButton.addEventListener("click", refreshGrid);
+
+const changeSize = document.querySelector("#changeGridSize");
+changeSize.addEventListener("click", changeGridSize);
+
+// ~~~ Only draw on mouse drag
+document.addEventListener("mousedown", () => {
+  document.addEventListener("mousemove", handleMouseMove);
+});
+
+// stop coloring on mouse drag
+document.addEventListener("mouseup", () => {
+  document.removeEventListener("mousemove", handleMouseMove);
+});
+
+// ~~~ Handle random color selection
+const randomButton = document.querySelector("#random");
+randomButton.addEventListener("click", handleSelection);
+
+// ~~~ Handle black selection
+const blackButton = document.querySelector("#black");
+blackButton.addEventListener("click", (e) => handleSelection(e, "black"));
